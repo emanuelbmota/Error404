1a7146ec9b21d3e0e9a12c1c10a332aa949c2767
emanuelbmota <emanuel.mota03@gmail.com>
2019-06-06 06:18:31 -0300

    testes

---
+++ login/js/main2.js
@@ -0,0 +1,13 @@
+$(".rslides").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 5000,          // Integer: Time between slide transitions, in milliseconds
+});
+
+$(".rslides_portfolio").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+  pager: true,           // Boolean: Show pager, true or false
+});
+
